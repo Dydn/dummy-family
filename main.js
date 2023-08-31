315c32f9ca570de30463a9e67f5c2f0caf7c9f3a
DyDn <126629784+Dydn@users.noreply.github.com>
2023-08-31 16:46:02 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -17,7 +17,21 @@ function take_snapshot(){
 
 console.log('ml5 version:', ml5.version);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/RI9QsREvJ/model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/igg1ii9c7/model.json',modelLoaded);
 function modelLoaded(){
     console.log("modelLoaded")
 }
+function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+ }
+
+ function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+        }
+ }
